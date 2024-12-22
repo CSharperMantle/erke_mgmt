dff2ee55d22ae235ba053b177a80d32566802ba0
astrangep <2659045971@qq.com>
2024-12-22 23:56:47 +0800

    pdm(proc): add p_initiate_checkin pdm(proc): fix p_signup

--- sql/proc.sql
+++ sql/proc.sql
@@ -57,7 +57,6 @@ BEGIN
   INSERT INTO SignUp(student_id, activity_id, signup_time) VALUES(student_id_, activity_id_ ,CURRENT_TIMESTAMP);
   okay_ := TRUE;
   msg_ := '';
-  RETURN; 
 END;
 
 CREATE OR REPLACE PROCEDURE p_initiate_checkin(
@@ -69,7 +68,38 @@ CREATE OR REPLACE PROCEDURE p_initiate_checkin(
   OUT code_ VARCHAR) AS
 DECLARE
 BEGIN
-  RAISE EXCEPTION 'not implemented';
+  okay_ := FALSE;
+  IF NOT EXISTS (
+    SELECT 1 FROM Activity a
+    WHERE a.organizer_id_=organizer_id_ AND a.activity_id=activity_id_
+  ) THEN
+    msg_ := 'not the same organizer';
+    RETURN;
+  END IF;
+
+  IF EXISTS (
+    SELECT 1 FROM Activity a
+    WHERE a.activity_id=activity_id_ AND CURRENT_TIMESTAMP<activity_start_time
+  ) THEN
+    msg_ := 'the activity is not started yet';
+    RETURN;
+  END IF;
+
+  IF NOT EXISTS (
+    SELECT 1 FROM Activity a
+    WHERE a.activity_id=activity_id_ AND a.activity_state IN (0,1)
+  ) THEN
+    msg_ := 'the activity is not in the check-in period';
+    RETURN;
+  END IF;
+  code := f_gen_random_checkinout_code();
+  INSERT INTO InitiateCheckIn (
+    organizer_id, activity_id, initiatecheckin_time, initiatecheckin_secret, initiatecheckin_valid_duration
+  ) VALUES (
+    organizer_id_, activity_id_, CURRENT_TIMESTAMP, code, valid_duration_
+  );
+  okay_ := TRUE;
+  msg_ := '';
 END;
 
 CREATE OR REPLACE PROCEDURE p_initiate_checkout(
