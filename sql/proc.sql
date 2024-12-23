b94a466f301c6f491f84920b4d6e63d8cfad3977
Rong "Mantle" Bao <baorong2005@126.com>
2024-12-23 10:37:29 +0800

    pdm(proc): add p_do_checkin
    Implements #12.

--- sql/proc.sql
+++ sql/proc.sql
@@ -37,8 +37,33 @@ CREATE OR REPLACE PROCEDURE p_do_checkin(
   OUT okay_ BOOLEAN,
   OUT msg_ VARCHAR) AS
 DECLARE
+  v RECORD;
+  t TIMESTAMP;
 BEGIN
-  RAISE EXCEPTION 'not implemented';
+  t := CURRENT_TIMESTAMP;
+  okay_ := FALSE;
+  FOR v IN (
+    SELECT s.student_id AS student_id, s.activity_id AS activity_id FROM SignUp s
+    INNER JOIN Activity a ON s.activity_id=a.activity_id
+    WHERE (
+      f_check_session_user_is('student', s.student_id) AND EXISTS (
+        SELECT 1 FROM InitiateCheckIn i
+        WHERE (
+          (i.activity_id=s.activity_id)
+          AND (i.initiatecheckin_secret=code_)
+          AND (t BETWEEN i.initiatecheckin_time AND (i.initiatecheckin_time + i.initiatecheckin_valid_duration))
+        )
+      )
+    )
+  ) LOOP
+    okay_ := TRUE;
+    INSERT INTO DoCheckIn(student_id, activity_id, docheckin_time) VALUES (v.student_id, v.activity_id, t);
+  END LOOP;
+  IF (okay_) THEN
+    msg_ := '';
+  ELSE
+    msg_ := 'invalid check-in code';
+  END IF;
 END;
 
 CREATE OR REPLACE PROCEDURE p_do_checkout(
