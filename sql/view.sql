52dfaf7f5c3c904e39a9d5448788798df883e808
Rong "Mantle" Bao <baorong2005@126.com>
2024-12-23 10:11:12 +0800

    pdm(view): add v_RateAgg
    Implements #17.

--- sql/view.sql
+++ sql/view.sql
@@ -1,11 +1,9 @@
 DROP VIEW IF EXISTS v_RateAgg;
 CREATE VIEW v_RateAgg AS
   SELECT
-    NULL::INT4 AS activity_id,
-    NULL::INT4 AS rate_cnt,
-    NULL::DECIMAL AS rate_avg,
-    NULL::DECIMAL AS rate_max,
-    NULL::DECIMAL AS rate_min;
+    r.activity_id AS activity_id, COUNT(r.rate_value) AS rate_cnt, AVG(r.rate_value) AS rate_avg, MAX(r.rate_value) AS rate_max, MIN(r.rate_value) AS rate_min
+  FROM Rate r
+  GROUP BY r.activity_id;
 
 DROP VIEW IF EXISTS v_ActivitySignUpCount;
 CREATE VIEW v_ActivitySignUpCount AS
