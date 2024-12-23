d752b771c440dd6c85a634658a5683a1f072c467
Rong "Mantle" Bao <baorong2005@126.com>
2024-12-23 15:20:04 +0800

    backend: add sample database connector

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,28 @@
-#[derive(rocket_db_pools::Database)]
-#[database("erke")]
-struct ErkeDb(rocket_db_pools::sqlx::PgPool);
+use sqlx::Connection;
 
-#[rocket::get("/")]
-fn route_index() -> &'static str {
-    "Hello, world!"
+type RouteResult<T, E = rocket::response::Debug<sqlx::Error>> = std::result::Result<T, E>;
+
+#[rocket::get("/hello")]
+async fn route_index() -> RouteResult<String> {
+    Ok("Hello, world!".to_string())
+}
+
+#[rocket::get("/add?<v1>&<v2>")]
+async fn route_add(v1: i32, v2: i32) -> RouteResult<String> {
+    let mut conn = sqlx::postgres::PgConnection::connect("postgres://erke_admin:Database%40123@127.0.0.1:15432/erke")
+        .await?;
+    let row: (i32,) = sqlx::query_as("SELECT $1 + $2;")
+        .bind(v1)
+        .bind(v2)
+        .fetch_one(&mut conn)
+        .await?;
+    conn.close().await?;
+    Ok(format!("{}", row.0).to_string())
 }
 
 #[rocket::launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", rocket::routes![route_index])
+    rocket::build()
+        .mount("/test", rocket::routes![route_index])
+        .mount("/test", rocket::routes![route_add])
 }
