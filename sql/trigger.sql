ed31d7b0259c84d15877cf150e40cc4b27b7352d
Napoleon231640 <163222827+Napoleon231640@users.noreply.github.com>
2024-12-27 15:28:14 +0800

    Update trigger.sql

--- sql/trigger.sql
+++ sql/trigger.sql
@@ -44,10 +44,18 @@ EXECUTE PROCEDURE tf_initiatecheckin_insert_update_activity_state();
 
 CREATE OR REPLACE FUNCTION tf_initiatecheckout_insert_check_activity_state() RETURNS TRIGGER AS $$
 BEGIN
-  RAISE WARNING 'not implemented';
-  RETURN NEW;
+    IF NOT EXISTS (
+        SELECT 1 FROM Activity
+        WHERE activity_id = NEW.activity_id AND activity_state IN (2, 3)
+    ) THEN
+        RAISE EXCEPTION 'The activity is not in the Started check-in or check-out state.';
+    END IF;
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+CREATE TRIGGER t_initiatecheckout_insert_check_activity_state BEFORE INSERT
+ON InitiateCheckOut FOR EACH ROW
+EXECUTE PROCEDURE tf_initiatecheckout_insert_check_activity_state();
 
 CREATE TRIGGER t_initiatecheckout_insert_check_activity_state BEFORE INSERT
 ON InitiateCheckOut FOR EACH ROW
@@ -66,10 +74,18 @@ EXECUTE PROCEDURE tf_initiatecheckout_insert_update_activity_state();
 
 CREATE OR REPLACE FUNCTION tf_audit_insert_check_activity_state() RETURNS TRIGGER AS $$
 BEGIN
-  RAISE WARNING 'not implemented';
-  RETURN NEW;
+    IF NOT EXISTS (
+        SELECT 1 FROM Activity
+        WHERE activity_id = NEW.activity_id AND activity_state = 2
+    ) THEN
+        RAISE EXCEPTION 'The activity is not in the Started check-in state.';
+    END IF;
+    RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
+CREATE TRIGGER t_audit_insert_check_activity_state BEFORE INSERT
+ON "Audit" FOR EACH ROW
+EXECUTE PROCEDURE tf_audit_insert_check_activity_state();
 
 CREATE TRIGGER t_audit_insert_check_activity_state BEFORE INSERT
 ON "Audit" FOR EACH ROW
