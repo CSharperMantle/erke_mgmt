39389a2f3fd966117790f68b658c1550051a28da
astrangep <2659045971@qq.com>
2024-12-28 18:14:18 +0800

    pdm(view):add v_ActivitySignUpCount add v_AvailActivity

--- sql/view.sql
+++ sql/view.sql
@@ -10,13 +10,20 @@ CREATE VIEW v_RateAgg AS
 DROP VIEW IF EXISTS v_ActivitySignUpCount;
 CREATE VIEW v_ActivitySignUpCount AS
   SELECT
-    NULL::INT4 AS activity_id,
-    NULL::INT4 AS cnt;
+    s.activity_id AS activity_id, COUNT(s.student_id) AS cnt 
+  FROM SignUp s
+  GROUP BY activity_id;
 
 DROP VIEW IF EXISTS v_AvailActivity;
 CREATE VIEW v_AvailActivity AS
   SELECT
-    NULL::INT4 AS activity_id;
+    a.activity_id AS activity_id
+  FROM Activity a
+  JOIN BeOpenTo b ON a.activity_id=b.activity_id
+  JOIN Student s ON b.grade_value=s.grade_value
+  WHERE s.student_id=SESSION_USER
+  AND CURRENT_TIMESTAMP BETWEEN a.activity_signup_start_time AND a.activity_signup_end_time;
+
 
 DROP VIEW IF EXISTS v_NonParticipActivity;
 CREATE VIEW v_NonParticipActivity AS
