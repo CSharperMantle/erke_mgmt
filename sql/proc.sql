9493b1162e81c60974f6f1bc4fc97caf35466fb9
Napoleon231640 <163222827+Napoleon231640@users.noreply.github.com>
2024-12-28 19:21:37 +0800

    add p_do_checkout

--- sql/proc.sql
+++ sql/proc.sql
@@ -71,33 +71,44 @@ BEGIN
 END;
 
 CREATE OR REPLACE PROCEDURE p_do_checkout(
-    code_ VARCHAR,
-    OUT okay_ BOOLEAN,
-    OUT msg_ VARCHAR
-) AS $$
+  code_ VARCHAR,
+  OUT okay_ BOOLEAN,
+  OUT msg_ VARCHAR) AS
 DECLARE
-    current_time TIMESTAMP := CURRENT_TIMESTAMP;
-    activity_id_ INTEGER;
-    student_id CHAR(10);
+  v RECORD;
+  t TIMESTAMP;
 BEGIN
-    SELECT activity_id, student_id INTO activity_id_, student_id
-    FROM DoCheckIn
-    WHERE docheckin_time <= current_time AND activity_id IN (
-        SELECT activity_id FROM InitiateCheckOut WHERE initiatecheckout_time <= current_time AND initiatecheckout_secret = code_
-    ) LIMIT 1;
-        IF EXISTS (SELECT 1 FROM InitiateCheckOut WHERE activity_id = activity_id_ AND initiatecheckout_time <= current_time AND initiatecheckout_secret = code_) THEN
-            INSERT INTO DoCheckout (student_id, activity_id, docheckout_time)
-            VALUES (student_id, activity_id_, current_time);
-            
-            okay_ := TRUE;
-            msg_ := 'The sign-out is successful.';
-        ELSE
-            okay_ := FALSE;
-            msg_ := 'The conditions for signing out haven't met';
-        END IF;
-    END IF;
+  t := CURRENT_TIMESTAMP;
+  okay_ := FALSE;
+  FOR v IN (
+    SELECT s.student_id AS student_id, s.activity_id AS activity_id FROM SignUp s
+    INNER JOIN Activity a ON s.activity_id = a.activity_id
+    WHERE (
+      a.activity_state = 2 
+      AND f_check_session_user_is('student', s.student_id)
+      AND EXISTS (
+        SELECT 1 FROM InitiateCheckOut i
+        WHERE (
+          (i.activity_id = s.activity_id)
+          AND (i.initiatecheckout_secret = code_)
+          AND (t BETWEEN i.initiatecheckout_time AND (i.initiatecheckout_time + i.initiatecheckout_valid_duration))
+        )
+      )
+      AND EXISTS (
+        SELECT 1 FROM DoCheckIn dci
+        WHERE (dci.student_id = s.student_id) AND (dci.activity_id = s.activity_id)
+      )
+    )
+  ) LOOP
+    okay_ := TRUE;
+    INSERT INTO DoCheckOut(student_id, activity_id, docheckout_time) VALUES (v.student_id, v.activity_id, t);
+  END LOOP;
+  IF (okay_) THEN
+    msg_ := '';
+  ELSE
+    msg_ := 'invalid checkout code';
+  END IF;
 END;
-$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE p_audit(
   auditor_id_ INTEGER,
