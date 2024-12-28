2866060c89545a50377ba476f907728849669023
Napoleon231640 <163222827+Napoleon231640@users.noreply.github.com>
2024-12-28 19:35:59 +0800

    add p_initiate_checkout

--- sql/proc.sql
+++ sql/proc.sql
@@ -20,46 +20,51 @@ BEGIN
   RAISE EXCEPTION 'not implemented';
 END;
 
-CREATE OR REPLACE FUNCTION gen_random_string(length_ INTEGER)
-RETURNS VARCHAR AS $$
-DECLARE
-    digits VARCHAR(10) := '0123456789';
-    random_string VARCHAR(4) := '';
-    i INTEGER;
-BEGIN
-    FOR i IN 1..length_ LOOP
-        random_string := random_string || SUBSTRING(digits, 1 + FLOOR(RANDOM() * length(digits)), 1);
-    END LOOP;
-    RETURN random_string;
-END;
-$$ LANGUAGE plpgsql;
 CREATE OR REPLACE PROCEDURE p_initiate_checkout(
-    organizer_id_ INTEGER,
-    activity_id_ INTEGER,
-    valid_duration_ TIME,
-    OUT okay_ BOOLEAN,
-    OUT msg_ VARCHAR,
-    OUT code_ VARCHAR
-) AS $$
+  organizer_id_ INTEGER,
+  activity_id_ INTEGER,
+  valid_duration_ TIME,
+  OUT okay_ BOOLEAN,
+  OUT msg_ VARCHAR,
+  OUT code_ VARCHAR) AS
 DECLARE
-    current_time TIMESTAMP := CURRENT_TIMESTAMP;
-    activity_state INTEGER;
 BEGIN
-    SELECT activity_state INTO activity_state
-    FROM Activity
-    WHERE activity_id = activity_id_ AND organizer_id = organizer_id_ AND activity_start_time <= current_time AND activity_state = 1;
-    IF NOT FOUND THEN
-        okay_ := FALSE;
-        msg_ := 'The conditions for signing out haven't met.';
-    ELSE
-        code_ := gen_random_string(8);
-        INSERT INTO InitiateCheckOut (activity_id, organizer_id, initiatecheckout_time, initiatecheckout_secret, initiatecheckout_valid_duration)
-        VALUES (activity_id_, organizer_id_, current_time, code_, valid_duration_);
-        okay_ := TRUE;
-        msg_ := 'The sign-out is successful.';
-    END IF;
+  okay_ := FALSE;
+  IF NOT EXISTS (
+    SELECT 1 FROM Activity a
+    WHERE a.organizer_id = organizer_id_ AND a.activity_id = activity_id_
+  ) THEN
+    msg_ := 'not the same organizer';
+    RETURN;
+  END IF;
+
+  IF EXISTS (
+    SELECT 1 FROM Activity a
+    WHERE a.activity_id = activity_id_ AND CURRENT_TIMESTAMP < a.activity_start_time
+  ) THEN
+    msg_ := 'the activity is not started yet';
+    RETURN;
+  END IF;
+
+  IF NOT EXISTS (
+    SELECT 1 FROM Activity a
+    WHERE a.activity_id = activity_id_ AND a.activity_state IN (1, 2)
+  ) THEN
+    msg_ := 'the activity is not in the check-out period';
+    RETURN;
+  END IF;
+
+  code_ := f_gen_random_checkinout_code();
+
+  INSERT INTO InitiateCheckOut (
+    organizer_id, activity_id, initiatecheckout_time, initiatecheckout_secret, initiatecheckout_valid_duration
+  ) VALUES (
+    organizer_id_, activity_id_, CURRENT_TIMESTAMP, code_, valid_duration_
+  );
+
+  okay_ := TRUE;
+  msg_ := '';
 END;
-$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE PROCEDURE p_do_checkin(
   code_ VARCHAR,
