0c04fa78eb1ad2ee256c0cee90194d2b8215421b
Rong "Mantle" Bao <baorong2005@126.com>
2025-01-01 11:35:54 +0800

    backend: add static server for frontend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,8 +2,13 @@ mod route;
 
 #[rocket::launch]
 fn rocket() -> _ {
-    rocket::build().mount(
-        "/api",
-        rocket::routes![route::login::route_login, route::logout::route_logout],
-    )
+    rocket::build()
+        .mount(
+            "/api",
+            rocket::routes![route::login::route_login, route::logout::route_logout],
+        )
+        .mount(
+            "/",
+            rocket::fs::FileServer::from(dotenvy_macro::dotenv!("FRONTEND_PATH")),
+        )
 }
