6de51d4c25624dca859d11727301dc40aa6c5f3c
Rong "Mantle" Bao <baorong2005@126.com>
2025-01-03 09:04:40 +0800

    pdm(role): add boilerplate role specs

---
+++ sql/role.sql
@@ -0,0 +1,20 @@
+CREATE ROLE erke_student NOLOGIN PASSWORD 'Nop@ssw0rd';
+
+GRANT CONNECT ON DATABASE erke TO erke_student;
+GRANT USAGE ON SCHEMA public TO erke_student;
+GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO erke_student;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO erke_student;
+
+CREATE ROLE erke_organizer NOLOGIN PASSWORD 'Nop@ssw0rd';
+
+GRANT CONNECT ON DATABASE erke TO erke_organizer;
+GRANT USAGE ON SCHEMA public TO erke_organizer;
+GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO erke_organizer;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO erke_organizer;
+
+CREATE ROLE erke_auditor NOLOGIN PASSWORD 'Nop@ssw0rd';
+
+GRANT CONNECT ON DATABASE erke TO erke_auditor;
+GRANT USAGE ON SCHEMA public TO erke_auditor;
+GRANT SELECT, INSERT, UPDATE ON ALL TABLES IN SCHEMA public TO erke_auditor;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO erke_auditor;
---
+++ sql/test_user.sql
@@ -0,0 +1,16 @@
+CREATE ROLE erke_student_23010001 LOGIN PASSWORD 'Database@123';
+GRANT erke_student TO erke_student_23010001;
+CREATE ROLE erke_student_23010002 LOGIN PASSWORD 'Database@123';
+GRANT erke_student TO erke_student_23010002;
+CREATE ROLE erke_student_20010001 LOGIN PASSWORD 'Database@123';
+GRANT erke_student TO erke_student_20010001;
+
+CREATE ROLE erke_organizer_90001 LOGIN PASSWORD 'Database@123';
+GRANT erke_organizer TO erke_organizer_90001;
+CREATE ROLE erke_organizer_90002 LOGIN PASSWORD 'Database@123';
+GRANT erke_organizer TO erke_organizer_90002;
+
+CREATE ROLE erke_auditor_10001 LOGIN PASSWORD 'Database@123';
+GRANT erke_auditor TO erke_auditor_10001;
+CREATE ROLE erke_auditor_10002 LOGIN PASSWORD 'Database@123';
+GRANT erke_auditor TO erke_auditor_10002;
